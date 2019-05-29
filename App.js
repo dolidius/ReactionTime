6399b7e2c2b361a360d115699811c19ce687f328
dolidius <dolidius@gmail.com>
2019-05-29 21:45:14 +0200

    Navigation and highscore improvements

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 import Home from './components/mainScreens/Home';
 import ReactionTest from './components/mainScreens/ReactionTest';
 import Highscores from './components/mainScreens/Highscores';
 
-const AppNavigator = createStackNavigator(
+const TabNavigator = createBottomTabNavigator(
     {
         Home,
-        ReactionTest,
         Highscores
+    },
+
+    {
+        defaultNavigationOptions: {
+            tabBarVisible: false
+        }
+    }
+)
+
+const AppNavigator = createStackNavigator(
+    {
+        TabNavigator,
+        ReactionTest,
     },
 
     {
--- components/mainScreens/Highscores.js
+++ components/mainScreens/Highscores.js
@@ -14,15 +14,24 @@ class Highscores extends Component {
             .then(req => JSON.parse(req))
             .then(json => scores = json)
             .catch(err => console.log(err));
-        
-        this.setState({ scores: scores.sort() });
+
+        scores.sort((a,b) => a > b);
+        scores = scores.slice(0, 10);
+
+        this.setState({ scores });
     }
 
     render(){
         if(this.state.scores){
             return(
                 <View style={styles.container}>
-                    {this.state.scores.map((score, i) => <Text key={i} style={{color: '#ffffff'}}>{score}</Text>)}
+                    <Text style={styles.title}>Highscore:</Text>
+                    {this.state.scores.map((score, i) => (
+                        <View key={i} style={{ flexDirection: 'row' }}>
+                            <Text style={[styles.score, styles.count]}>{i + 1}. </Text>
+                            <Text style={styles.score}>{score}</Text>
+                        </View>
+                    ))}
                 </View>
             );
         }else{
@@ -41,7 +50,24 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#545454',
+    },
+
+    title: {
+        fontSize: 36,
+        color: '#ffffff',
+        marginBottom: 4
+    },
+
+    score: {
+        color: '#ffffff',
+        fontSize: 20,
+        width: '50%'
+    },
+
+    count: {
+        textAlign: 'right'
     }
+
 });
 
 export default Highscores;
